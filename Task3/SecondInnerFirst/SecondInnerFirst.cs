49f5ab5168c3fc5ad70a0922919f981e77ae4d74
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-06 11:55:45 +0300

    Changing method InsertMethod

--- Task3/SecondInnerFirst/SecondInnerFirst.cs
+++ Task3/SecondInnerFirst/SecondInnerFirst.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SecondInnerFirst
+namespace SecondInner
 {
     public static class SecondInnerFirst
     {
-        public static int InsertMethod()
+        private const int MaxBit = 30;
+        /// <summary>
+        /// The method for inserting the second number of the first.
+        /// </summary>
+        /// <param name="first">The first value.</param>
+        /// <param name="second">The second value.</param>
+        /// <param name="endPos">Start number of bit.</param>
+        /// <param name="startPos">End number of bit.</param>
+        /// <returns></returns>
+        public static int InsertMethod(int first, int second, int endPos, int startPos)
         {
-            return 1;
+            if (startPos < 0 || endPos > MaxBit)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (endPos < startPos)
+                throw new ArgumentException();
+
+            int sizeInsert = (int)(Math.Pow(2, endPos - startPos + 1) - 1);
+            second = second & sizeInsert;
+            second = second << startPos;
+
+            return first | second;
+
         }
     }
 }
---
+++ Task3/Task3.Test/SecondInnerFirstTest.cs
@@ -0,0 +1,19 @@
+﻿using SecondInner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task3.Test
+{
+    public class SecondInnerFirstTest
+    {
+        [TestCase()]
+        int InsertMethodTestWithNormalArgument(int first, int second, int startPos, int endPos)
+        {
+            return SecondInnerFirst.InsertMethod(first, second, startPos, endPos);
+        }
+    }
+}
